0405c7944610346cd23505780c9579f5bf49aa9e
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-09 21:34:36 -0500

    feat: Created Currency Controller and imported

---
+++ src/presentation/controllers/currency.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CurrencyInputDto } from 'src/domain/dto/currency-input.dto';
+import { CurrencyService } from '../services/currency.service';
+
+@Controller()
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  @Post()
+  exchangeCurrency(@Body() body: CurrencyInputDto) {
+    return this.currencyService.exchangeCurrency(body);
+  }
+}
--- src/presentation/currency.module.ts
+++ src/presentation/currency.module.ts
@@ -1,8 +1,10 @@
+import { CurrencyController } from './controllers/currency.controller';
+import { CurrencyService } from './services/currency.service';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [CurrencyController],
+  providers: [CurrencyService],
 })
 export class CurrencyModule {}
