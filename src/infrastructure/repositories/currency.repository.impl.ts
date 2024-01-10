76d2227224feee776f6156749734db66cb7386de
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-09 21:52:02 -0500

    feat: Injecting values into interfaces from providers

--- src/infrastructure/repositories/currency.repository.impl.ts
+++ src/infrastructure/repositories/currency.repository.impl.ts
@@ -1,9 +1,11 @@
+import { Inject, Injectable } from '@nestjs/common';
 import { CurrencyInputDto } from 'src/domain/dto/currency-input.dto';
 import { CurrencyEntity } from 'src/domain/entities/currency.entity';
 import { CurrencyRepository } from 'src/domain/repositories/currency.repository';
 
+@Injectable()
 export class CurrencyRepositoryImpl implements CurrencyRepository {
-  constructor(private readonly currencyRepository: CurrencyRepository) {}
+  constructor(@Inject('CurrencyDatasource') private readonly currencyRepository: CurrencyRepository) {}
 
   exchangeCurrency(currencyInput: CurrencyInputDto): CurrencyEntity {
     return this.currencyRepository.exchangeCurrency(currencyInput);
