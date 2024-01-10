15679633354e78c340ad7541cef7da55b38d2b5c
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-10 00:16:33 -0500

    feat: Added Validator Decorator into CurrencyInputDto

--- src/domain/dto/currency-input.dto.ts
+++ src/domain/dto/currency-input.dto.ts
@@ -1,5 +1,25 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
 export class CurrencyInputDto {
+  @Min(0, { message: 'Debe ingresar un valor mayor o igual a 0' })
+  @IsNumber(
+    { allowInfinity: false, allowNaN: false },
+    { message: 'El monto ingresado debe ser numérico' },
+  )
+  @IsNotEmpty({ message: 'Debe ingresar el monto' })
   amount: number;
+
+  @IsString({ message: 'Debe ingresar un texto en la moneda de origen' })
+  @IsNotEmpty({ message: 'Debe ingresar una moneda de origen' })
   originCurrency: string;
+
+  @IsString({ message: 'Debe ingresar un texto en la moneda de destino' })
+  @IsNotEmpty({ message: 'Debe ingresar una moneda de destino' })
   destinyCurrency: string;
 }
