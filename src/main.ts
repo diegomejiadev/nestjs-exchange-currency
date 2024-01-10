1875f7250b6fd43ce3616a42fdc6271fba2eaf1b
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-10 00:16:00 -0500

    chore: Added ValidationPipe into main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     CurrencyModule,
     new FastifyAdapter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      
+    }),
   );
   await app.listen(3000);
 }
