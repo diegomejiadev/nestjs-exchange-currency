8495281a2e304e2e3b34fdcc17d831e612163766
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-09 21:51:29 -0500

    feat: Added values into interface classes

--- src/presentation/currency.module.ts
+++ src/presentation/currency.module.ts
@@ -1,10 +1,22 @@
+import { CurrencyRepositoryImpl } from 'src/infrastructure/repositories/currency.repository.impl';
 import { CurrencyController } from './controllers/currency.controller';
 import { CurrencyService } from './services/currency.service';
 import { Module } from '@nestjs/common';
+import { MockCurrencyDatasourceImpl } from 'src/infrastructure/datasources/mock-currency.datasource.impl';
 
 @Module({
   imports: [],
   controllers: [CurrencyController],
-  providers: [CurrencyService],
+  providers: [
+    CurrencyService,
+    {
+      provide: 'CurrencyRepository',
+      useClass: CurrencyRepositoryImpl,
+    },
+    {
+      provide: 'CurrencyDatasource',
+      useClass: MockCurrencyDatasourceImpl,
+    },
+  ],
 })
 export class CurrencyModule {}
