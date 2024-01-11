d3ccb8efce19aaf58817063ce35d478bba42656f
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-11 08:35:26 -0500

    feat: Implemented Swagger and added global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,29 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import { swaggerSetup } from './utils/swagger';
 
 async function bootstrap() {
+  //* Indicamos que va a estar levantado en Fastify
   const app = await NestFactory.create<NestFastifyApplication>(
     CurrencyModule,
     new FastifyAdapter(),
   );
+
+  // * Agregamos un Pipe para la validación de DTOs
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  //* Ingresamos el prefijo global para llamar a la API
+  app.setGlobalPrefix('/api');
+
+  //* Llamamos la funcion que activa Swagger
+  swaggerSetup(app);
+
+  //* Indicamos el Puerto en el que va a escuchar el servidor
   await app.listen(3000);
 }
 bootstrap();
