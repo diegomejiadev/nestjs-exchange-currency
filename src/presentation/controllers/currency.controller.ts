05724071dfbe494ad7dc54fa2f67942b52e8ee91
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-11 09:33:30 -0500

    feat: Added JWT Auth guard into currency controller

--- src/presentation/controllers/currency.controller.ts
+++ src/presentation/controllers/currency.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CurrencyInputDto } from 'src/domain/dto/currency-input.dto';
 import { CurrencyService } from '../services/currency.service';
+import { JwtAuthGuard } from '../guards/jwt.guard';
 
 @Controller('currency')
 export class CurrencyController {
   constructor(private readonly currencyService: CurrencyService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   exchangeCurrency(@Body() body: CurrencyInputDto) {
     return this.currencyService.exchangeCurrency(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   loadAllCurrencies() {
     return this.currencyService.loadAllCurrencies();
