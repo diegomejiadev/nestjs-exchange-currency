6f2ab362daec10163bb4a9a183c4410849341337
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-11 08:40:58 -0500

    feat: Added Swagger decorators into DTO

--- src/domain/dto/currency-input.dto.ts
+++ src/domain/dto/currency-input.dto.ts
@@ -1,12 +1,12 @@
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsPositive,
-  IsString,
-  Min,
-} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CurrencyInputDto {
+  @ApiProperty({
+    description: 'Valor inicial que va a ser convertido',
+    default: 10,
+    type: Number,
+  })
   @Min(0, { message: 'Debe ingresar un valor mayor o igual a 0' })
   @IsNumber(
     { allowInfinity: false, allowNaN: false },
@@ -15,10 +15,18 @@ export class CurrencyInputDto {
   @IsNotEmpty({ message: 'Debe ingresar el monto' })
   amount: number;
 
+  @ApiProperty({
+    description: 'Tipo de moneda de origen',
+    default: 'USD',
+  })
   @IsString({ message: 'Debe ingresar un texto en la moneda de origen' })
   @IsNotEmpty({ message: 'Debe ingresar una moneda de origen' })
   originCurrency: string;
 
+  @ApiProperty({
+    description: 'Tipo de moneda de destino',
+    default: 'PEN',
+  })
   @IsString({ message: 'Debe ingresar un texto en la moneda de destino' })
   @IsNotEmpty({ message: 'Debe ingresar una moneda de destino' })
   destinyCurrency: string;
