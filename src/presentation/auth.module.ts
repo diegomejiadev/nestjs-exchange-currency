28272bb3610f1e9ce5f834126582114a97016341
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-11 09:32:17 -0500

    chore: Created Auth Module

---
+++ src/presentation/auth.module.ts
@@ -0,0 +1,22 @@
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET_VALUE,
+      signOptions: { expiresIn: '10m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+})
+export class AuthModule {}
