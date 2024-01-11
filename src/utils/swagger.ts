1ad0661d39a3ee61eed5d5f015dda1afa38ac1f4
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-11 08:35:14 -0500

    chore: Created function to create Swagger document

---
+++ src/utils/swagger.ts
@@ -0,0 +1,15 @@
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerSetup = (app: NestFastifyApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Joyit Exchange Currency')
+    .setDescription(
+      'Proyecto desarrollado en NestJS para la conversión de divisas',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
