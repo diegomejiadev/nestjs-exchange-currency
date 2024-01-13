ba71ef3823779521563a1619f5790b6695e116a3
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-13 06:49:54 -0500

    chore: Added ApiProperty decorators into entity classes

--- src/domain/entities/currency.entity.ts
+++ src/domain/entities/currency.entity.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CurrencyEntity {
+  @ApiProperty()
   code: string;
+
+  @ApiProperty()
   updated_at: string;
+
+  @ApiProperty()
   data: Record<
     string,
     {
--- src/domain/entities/exchange-currency.entity.ts
+++ src/domain/entities/exchange-currency.entity.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ExchangeCurrencyEntity {
+  @ApiProperty()
   amount: number;
+
+  @ApiProperty()
   baseAmount: number;
+
+  @ApiProperty()
   originCurrency: string;
+
+  @ApiProperty()
   destinyCurrency: string;
+
+  @ApiProperty()
   exchangeType: string;
+
+  @ApiProperty()
   lastUpdated: string;
 }
