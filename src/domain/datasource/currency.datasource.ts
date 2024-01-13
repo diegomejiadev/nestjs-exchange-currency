160ddb205cbfbe165976999eb9b448d07c69d862
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-13 06:26:20 -0500

    chore: Added function into Currency interfaces

--- src/domain/datasource/currency.datasource.ts
+++ src/domain/datasource/currency.datasource.ts
@@ -1,7 +1,19 @@
 import { ExchangeCurrencyInputDto } from '../dto/exchange-currency-input.dto';
+import { UpdateCurrencyInputDto } from '../dto/update-currency-input.dto';
+import { CurrencyEntity } from '../entities/currency.entity';
 import { ExchangeCurrencyEntity } from '../entities/exchange-currency.entity';
 
 export interface CurrencyDataSource {
-  exchangeCurrency(currencyInput: ExchangeCurrencyInputDto): Promise<ExchangeCurrencyEntity>;
+  //* Funcion encargada de realizar el cambio de moneda
+  exchangeCurrency(
+    currencyInput: ExchangeCurrencyInputDto,
+  ): Promise<ExchangeCurrencyEntity>;
+
+  //* Funcion que carga los tipos de moneda a la in-memory database
   loadAllCurrencies(): Promise<boolean>;
+
+  //* Actualiza el valor de un tipo de moneda
+  updateCurrency(
+    updateCurrencyInputDto: UpdateCurrencyInputDto,
+  ): Promise<CurrencyEntity>;
 }
