647f9016b8a4ddbc960bdca89ff74799af80a67a
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-13 06:42:45 -0500

    docs: Added description into endpoints

--- src/presentation/controllers/auth.controller.ts
+++ src/presentation/controllers/auth.controller.ts
@@ -4,11 +4,16 @@ https://docs.nestjs.com/controllers#controllers
 
 import { Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    description: 'Método POST que retorna un JWT habilitado durante 10 minutos',
+  })
   @Post('login')
   async login() {
     return await this.authService.generateToken();
--- src/presentation/controllers/currency.controller.ts
+++ src/presentation/controllers/currency.controller.ts
@@ -2,26 +2,39 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ExchangeCurrencyInputDto } from 'src/domain/dto/exchange-currency-input.dto';
 import { CurrencyService } from '../services/currency.service';
 import { JwtAuthGuard } from '../guards/jwt.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UpdateCurrencyInputDto } from 'src/domain/dto/update-currency-input.dto';
 
+@ApiTags('Currency')
 @ApiBearerAuth()
 @Controller('currency')
 export class CurrencyController {
   constructor(private readonly currencyService: CurrencyService) {}
 
+  @ApiOperation({
+    description:
+      'Función encargada de la conversión de un tipo de moneda origen a un tipo de moneda destino',
+  })
   @UseGuards(JwtAuthGuard)
   @Post('exchange')
   exchangeCurrency(@Body() body: ExchangeCurrencyInputDto) {
     return this.currencyService.exchangeCurrency(body);
   }
 
+  @ApiOperation({
+    description:
+      'Función encargada de volver a cargar los valores por defecto de los tipos de moneda presentes',
+  })
   @UseGuards(JwtAuthGuard)
   @Post('load')
   loadAllCurrencies() {
     return this.currencyService.loadAllCurrencies();
   }
 
+  @ApiOperation({
+    description:
+      'Función encargada de actualizar un tipo de moneda con respecto a los valores con otro tipo de monedas',
+  })
   @UseGuards(JwtAuthGuard)
   @Post('update')
   updateCurrency(@Body() body: UpdateCurrencyInputDto) {
